d1be04f4c7a88f944b043801c9ab23cc4067e328
Lucas <97757814+lucaslukao@users.noreply.github.com>
2022-04-08 15:55:56 -0300

    Exercicios de Python

--- ex045.py
+++ ex045.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = randint(0, 2)
 print('''Suas opções:
@@ -6,34 +7,36 @@ print('''Suas opções:
 [1] - PAPEL
 [2] - TESOURA''')
 jogador = int(input('Qual a sua escolha? '))
-print('_-' * 10)
-print('''O computador jogou {}
-Jogador jogou {}'''.format(itens[computador], itens[jogador]))
-print('_-' * 10)
-if computador == 0: # O computador jogou pedra
-    if jogador == 0:
-        print('EMPATE.')
-    elif jogador == 1:
-        print('JOGADOR VENCE!!!')
-    elif jogador == 2:
-        print("COMPUTADOR VENCE!")
-    else:
-        print('Jogada INVÁLIDA!!! Tente novamente')
-elif computador == 1: # O computador jogou papel
-    if jogador == 0:
-        print('COMPUTADOR VENCE!')
-    elif jogador == 1:
-        print('EMPATE!')
-    elif jogador == 2:
-        print('JOGADOR VENCE!!!')
-    else:
-        print('JOGADA INVÁLIDA! Tente novamente.')
-elif computador == 2: # O computador jogou tesoura
-    if jogador == 0:
-        print('JOGADOR VENCE!!!')
-    elif jogador == 1:
-        print('COMPUTADOR VENCE!')
-    elif jogador == 2:
-        print('EMPATE!')
-    else:
-        print('JOGADA INVÁLIDA! Tente novamente.')
+if jogador <= 2:
+    print('JO')
+    sleep(1)
+    print('KEN')
+    sleep(1)
+    print('PO!!!')
+    sleep(1)
+    print('_-' * 20)
+    print('''O computador jogou {}\nJogador jogou {}'''.format(itens[computador], itens[jogador]))
+    print('_-' * 20)
+    if computador == 0:   # O computador jogou pedra
+        if jogador == 0:
+            print('EMPATE.')
+        elif jogador == 1:
+            print('JOGADOR VENCE!!!')
+        else:
+            print("COMPUTADOR VENCE!")
+    elif computador == 1:    # O computador jogou papel
+        if jogador == 0:
+            print('COMPUTADOR VENCE!')
+        elif jogador == 1:
+            print('EMPATE!')
+        else:
+            print('JOGADOR VENCE!!!')
+    elif computador == 2:   # O computador jogou tesoura
+        if jogador == 0:
+            print('JOGADOR VENCE!!!')
+        elif jogador == 1:
+            print('COMPUTADOR VENCE!')
+        else:
+            print('EMPATE!')
+else:
+    print('Jogada inválida!')
