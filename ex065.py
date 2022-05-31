cc855afa7462dbac0ecd41c0f31007e835c0d32b
Lucas <97757814+lucaslukao@users.noreply.github.com>
2022-05-31 16:37:30 -0300

    Python Exercícios

--- ex065.py
+++ ex065.py
@@ -1,5 +1,16 @@
-contn = 0
-contp = ['S']
-while contp == ['S']:
-    v = int(input('Digite um valor: '))
-    p = str(input('Deseja continuar?[S/N] ')).strip().upper()
+cont = conts = m = n = 0
+p = 'S'
+while p != 'N':
+    s = int(input('Digite um valor: '))
+    p = str(input('Deseja continuar?[S/N] ')).strip().upper()[0]
+    conts += s
+    cont += 1
+    if cont == 1:
+        m = n = s
+    else:
+        if s > m:
+            m = s
+        elif s < n:
+            n = s
+print('''A média entre os números digitados foi de {:.2f}
+O maoir valor foi {}, e o menor foi {}.'''.format(conts/cont, m, n))
