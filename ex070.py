26d418e5e4dbe1656bc4c984491f7fb68a3dda7c
Lucas <97757814+lucaslukao@users.noreply.github.com>
2022-07-05 16:55:56 -0300

    Python Exercícios

--- ex070.py
+++ ex070.py
@@ -1,15 +1,19 @@
-total = 0
+total =contm = contb = 0
 print('-' * 20)
 print('PROGRAMA DE LOJINHA')
 print('-' * 20)
 while True:
     nome = str(input('Qual o nome do produto? '))
     valor = float(input('Qual o preço do produto?R$ '))
+    if valor >= 1000:
+        contm += 1
     total += valor
     final = ' '
     while final not in 'SN':
         final = str(input('Deseja continuar?[S/N] ')).strip().upper()[0]
+    print('-' * 20)
     if final == 'N':
         print('FIM DO PROGRAMA!')
-        print(f'O total da compra é {total}')
+        print(f'O total da compra é {total}.')
+        print(f'{contm} custam mais de R$1000.00.')
         break
