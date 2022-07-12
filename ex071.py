fc02fcefdaff643bc6b3847ea4c5e59bbb108893
Lucas <97757814+lucaslukao@users.noreply.github.com>
2022-07-12 15:43:29 -0300

    Python Exercícios

--- ex071.py
+++ ex071.py
@@ -1,26 +1,25 @@
-cinquenta = vinte = dez = um = 0
 print('-' * 20)
 print('{:^20}'.format('CAIXA ELETRÔNICO'))
 print('-' * 20)
 valor = int(input('Qual valor deseja sacar?R$ '))
+total = valor
+ced = 50
+totced = 0
 while True:
-    if valor >= 50:
-        valor -= 50
-        cinquenta += 1
-    elif valor >= 20:
-        valor -= 20
-        vinte += 1
-    elif valor >= 10:
-        valor -= 10
-        dez += 1
-    elif valor >= 1:
-        valor -= 1
-        um += 1
-    elif valor == 0:
-        break
-print(f'Total de {cinquenta} cédulas de R$50.')
-print(f'Total de {vinte} cédulas de R$20.')
-print(f'Total de {dez} cédulas de R$10.')
-print(f'Total de {um} cédulas de R$1.')
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} de cédulas de R${ced}.')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
 print('-' * 20)
 print('Volte sempre!')
