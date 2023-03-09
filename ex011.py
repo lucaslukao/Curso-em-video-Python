9f9c2923b671b93f97b3f9bd4562dd36ef3af4af
Lucas <lm1955435@gmail.com>
2023-03-09 11:34:32 -0300

    Python Exercícios

--- ex011.py
+++ ex011.py
@@ -1,4 +1,3 @@
 h = float(input('Qual a altura da parede, em metros? '))
 l = float(input('Qual a largura da parede, em metros? '))
-print(f'A área da parede é {h*l:.1f}m e será necessários {(h*l)/2:.1f} litros de tinta para pintá-la.')
-
+print(f'A área da parede é {h * l:.1f}m² e será necessários {(h * l) / 2:.1f} litros de tinta para pintá-la.')
